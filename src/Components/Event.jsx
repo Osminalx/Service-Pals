5753b305b98d8d463d7d814ae055907f11dcd7cc
Osminalx <verdestro64@gmail.com>
2022-11-11 17:47:14 -0800

    Inicios

---
+++ src/Components/Event.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Event = () => {
+  return (
+    <div>
+        <div>
+            <img src="" alt="" />
+            <h2></h2>
+            <h6></h6>
+            <p></p>
+            <button></button>
+            <button></button>
+        </div>
+    </div>
+  )
+}
+
+export default Event
